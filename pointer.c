baa1a1cb91dc6e5c17fa95ac037a3df97751644d
gudlad <19a31@sdmit.in>
2022-05-23 16:27:03 +0530

    4th commit

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int *ptr;
+int a=4;
+ptr=&a;
+printf("ptr is a pointer that holds the address of a");
+printf("using *ptr we can print the value of a i.e %d",*ptr);
+return 0;
+
+}
