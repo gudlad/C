1e32195a99149d9d6e5c90280f01eb45abc298f3
gudlad <19a31@sdmit.in>
2022-05-23 23:37:57 +0530

    pointer2 commit

---
+++ pointer2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int age=22;
+int *ptr=&age;  // to store the adress of variable of type int age=22;
+// char * ptr   to the store address of variable of type char a='*';
+// float *ptr   to the  store the addresss of variable of type float a=0.3;
+int _age=*ptr;
+printf("%d\n",_age);
+// we can use the %p format specifire to print the address values(hexvalue)
+printf("%p\n",&age); // both value will be same
+printf("%p\n",ptr);
+printf("%p\n",&ptr); /// pointer will have its own address
+
+// to convert hex values to int we can use the %u (unsigned int) specifier
+printf("%u\n",&age); // both value will be same
+printf("%u\n",&ptr);
+printf("%u\n",ptr);
+return 0;
+}
